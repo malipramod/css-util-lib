50b3c4414c2fe56a2872975cab600d3dfedb3f5c
Pramod Mali <pramodmaali@gmail.com>
2022-03-19 10:49:04 +0530

    purge css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 const { src, dest, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const purgecss = require('gulp-purgecss');
 
 function buildStyles() {
     return src('partials/**/*.scss')
         .pipe(sass())
+        .pipe(purgecss({content: ['*.html']}))
         .pipe(dest('css'));
 }
 
